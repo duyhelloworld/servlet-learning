a6386954d81e6c0a71c2989f257f8e51ac361031
duyhelloworld <codedaovoiduy@gmail.com>
2022-12-02 21:26:27 +0700

    new file:   structureOfA_servletPage/src/main/java/com/servlet/Redirection.java

---
+++ structureOfA_servletPage/src/main/java/com/servlet/Redirection.java
@@ -0,0 +1,24 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/redirect...")
+public class Redirection extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = "http://localhost:8084/structure/form";
+        // + Way 1 :
+        // resp.sendRedirect();
+
+        // + Way 2 :
+        // resp.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+        resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        resp.setHeader("Location", url);
+    }
+}
